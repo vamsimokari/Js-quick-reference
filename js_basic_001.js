17217a838f988282965fc20b5413408828242ace
vamsimokari <vamsimokari@gmail.com>
2016-03-09 17:23:22 +0530

    Create js_basic_001.js

---
+++ js_basic_001.js
@@ -0,0 +1,17 @@
+var foo = "bar";
+
+function bar(){
+  var foo1 = "baz";
+  console.log(foo);
+  bam
+}
+
+function baz(foo){
+  console.log(foo+" "+ bam);
+  foo = "bam";
+  bam = "yay";
+  console.log(foo+" "+ bam);
+}
+
+bar();//??
+baz();//??
