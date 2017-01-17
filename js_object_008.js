b5a9588c9ec3f9b59426e7854db2aef718063b85
vamsimokari <vamsimokari@gmail.com>
2017-01-17 14:42:43 +0530

    Create js_object_008.js

---
+++ js_object_008.js
@@ -0,0 +1,169 @@
+
+{
+  "orders": [
+    {
+      "id": "1c4de896-7add-490e-8b28-7df7e8cd8fb3",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 200,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        },
+        {
+          "id": null,
+          "item": null,
+          "price": 200,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    },
+    {
+      "id": "6d6e841e-8285-49db-87f4-ce08ef78edb5",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 200,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        },
+        {
+          "id": null,
+          "item": null,
+          "price": 200,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    },
+    {
+      "id": "731e5c1b-cb1a-48f0-bcc3-4bf39cc0360c",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        },
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    },
+    {
+      "id": "8b88306b-03aa-426f-82ac-87ce9e6f1dc8",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        },
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    },
+    {
+      "id": "a88ef907-c5d7-44d2-b5bb-1a61b23d0fcc",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        },
+        {
+          "id": null,
+          "item": null,
+          "price": 34566,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    },
+    {
+      "id": "e5659898-8475-474f-8d1f-0b9410a8bfc3",
+      "status": null,
+      "orderItems": [
+        {
+          "id": null,
+          "item": null,
+          "price": 200,
+          "status": "INITIATED",
+          "shipByDate": null,
+          "dueDate": null,
+          "itemImage": "http://pimg2.bluestone.com/BV-R03_YAA18DIG6XXXXXXXX_ABCD00-pr-1024.jpg?version=0",
+          "itemName": "Ador Love Ruby Ring"
+        }
+      ],
+      "addedon": null,
+      "orderContact": null,
+      "orderShippingAddress": null
+    }
+  ]
+}
