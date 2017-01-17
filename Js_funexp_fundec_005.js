2d3ac05561a6040d5d01a38784a4639e24532d73
vamsimokari <vamsimokari@gmail.com>
2017-01-17 14:35:51 +0530

    Create Js_funexp_fundec_005.js

---
+++ Js_funexp_fundec_005.js
@@ -0,0 +1,76 @@
+// Code goes here
+
+// function foo(){//Declaration
+// 	foo();
+
+// } 
+
+
+// vs 
+
+
+// /*
+// 	Function Expression
+// */
+
+// var foo = function bar(){
+// 	bar();
+// }
+
+
+// var foo = function bar(){
+//   var foo = "baz";
+  
+//   function baz(foo){
+//     console.log(foo);
+//     foo = bar;
+//     console.log(foo);
+//   }
+  
+//   baz(foo);
+// }
+
+// foo();
+// bar();
+
+
+
+// function foo(){
+//     function bar() {
+//         return 3;
+//     }
+//     return bar();
+//     function bar() {
+//         return 8;
+//     }
+// }
+// alert(foo());
+
+
+// function foo(){
+//    console.log(bar);
+//     var bar = function() {
+//         return 3;
+//     };
+ 
+//     return bar();
+//     var bar = function() {
+//         return 8;
+//     };
+// }
+// alert(foo());
+
+
+
+
+// alert(foo());
+
+// function foo(){
+//     var bar = function() {
+//         return 3;
+//     };
+//     return bar();
+//     var bar = function() {
+//         return 8;
+//     };
+// }
